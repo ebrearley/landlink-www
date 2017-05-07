01c32e31e174df0fc2b773a1243bb514dd57a427
Eric Brearley <eric.r.brearley@gmail.com>
2017-05-07 12:15:09 +1000

    Wait for image to download before fading to parallax

--- assets/prepare-parallax.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  const hero = $('#hero');
-  hero.addClass('transition');
-  hero.removeClass('hero-background');
-
-  const isolation = $('#isolation')
-  isolation.addClass('transition');
-  isolation.removeClass('isolation-background');
-});
