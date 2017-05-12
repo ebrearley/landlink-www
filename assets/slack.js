653046cbadf3e0b06bed716abde72aa071fa5e6a
Eric Brearley <eric.r.brearley@gmail.com>
2017-05-12 15:10:39 +1000

    Simple landing page (#2)
    * Clear directory
    
    * Add Hero
    
    * Add bootstrap
    
    * Our farmers section
    
    * Isolation section
    
    * Your help section
    
    * line-height normal
    
    * Weekend section
    
    * Form section
    
    * The footer
    
    * Parallaxify
    
    * Tweaks to page loading
    
    * Finished testing slack, change to real slack channel
    
    * Viewport metatag
    
    * Lazy load javascript
    
    * Reduce indentation of javascript to two spaces
    
    * Asynchronously get stylesheets and roboto font
    
    * Fade the solid colour when parallax effect is loaded
    
    * Wait for image to download before fading to parallax
    
    * Add fallback fonts
    
    * Copy update
    
    * Another copy update
    
    * Copy update

---
+++ assets/slack.js
@@ -0,0 +1,40 @@
+$(function() {
+  $('#emailForm').on("submit",function(e) {
+    e.preventDefault(); // cancel the actual submit
+
+    const name = $('#frmFirstName').val();
+    const email = $('#frmEmailAddress').val();
+
+    const emojis = [
+      ':confetti_ball:',
+      ':tada:',
+      ':heart:',
+      ':heart_eyes:',
+    ];
+
+    const slackMessage = {
+        username: 'LandLink Website\'s friendly bot, Beep Boop',
+        icon_emoji: emojis[Math.floor(Math.random() * emojis.length)],
+        channel: '#website-subscriptions',
+        attachments: [{
+            fallback: `New mailing list subscription from ${name} with email: ${email}`,
+            title: 'New mailing list subscription!',
+            color: '#54CE5C',
+            pretext: 'Holla! :simple_smile: Make way, a new subscription rolling through!',
+            mrkdwn_in: ['text'],
+            text: `Name: ${name}${'\n'}Email: ${email}`,
+        }]
+    };
+
+    var payload = JSON.stringify(slackMessage);
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open('POST', 'https://hooks.slack.com/services/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', false);
+    xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xmlhttp.send(payload);
+
+    $('#frmFirstName').val('');
+    $('#frmEmailAddress').val('')
+    $('#cta-success').removeClass('hidden-block');
+  });
+});
